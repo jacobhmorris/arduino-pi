9ed7738056de47c16aa9b41542001199d7e7d7b5
jacobhmorris <jacobhmorris@gmail.com>
2012-12-03 22:20:10 +1100

    Added new version of Android app - implemented using cleaner architecture

---
+++ pi/piServer/piSerialHelper.py
@@ -0,0 +1,39 @@
+import serial
+import struct
+
+class piSerial:
+    def __init__(self,serialPort,serialBaud,useHex):
+        self.serialPort = serialPort
+        self.serialBaud = serialBaud
+        self.serialReady = False
+        
+        #set defaults if nothing provided
+        if(useHex != None):
+            self.useHex = useHex
+        else:
+            self.useHex = False
+        
+        try:
+            self.ser = serial.Serial(self.serialPort, self.serialBaud)
+            self.serialReady = True
+        except Exception, msg:
+            print("Error opening serial port: "+msg[0])
+            self.serialReady = False
+        
+        
+    
+    def C(self, val):
+        return struct.pack('H', val)
+    
+    #takes values for speed between 0 and 255, and either 0 or 1 for direction
+    def sendMotorValues(self, leftSpeed, rightSpeed, leftDirection, rightDirection):
+        if(self.useHex):
+            self.ser.write('#')
+            self.ser.write(self.C(leftSpeed) + self.C(rightSpeed) + self.C(leftDirection) + self.C(rightDirection))
+            
+        else:
+            self.ser.write('*')
+            self.ser.write(str(leftSpeed) + str(rightSpeed) + str(leftDirection) + str(rightDirection))
+
+    def isReady(self):
+        return self.serialReady
